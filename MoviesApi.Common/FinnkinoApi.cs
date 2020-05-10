538ea4bc73c177e58a0b54fff155209dc6203d7a
Antti Riksman <antti.riksman@zure.com>
2020-05-10 22:33:59 +0300

    Update FinnkinoApi.cs

--- MoviesApi.Common/FinnkinoApi.cs
+++ MoviesApi.Common/FinnkinoApi.cs
@@ -5,32 +5,30 @@ namespace MoviesApi.Common
 {
   public static class FinnkinoApi
   {
+    private static HttpClient httpClient = new HttpClient();
+
     private static async Task<HttpResponseMessage> GetAllAreas()
     {
       var url = "http://www.finnkino.fi/xml/TheatreAreas";
-      using var client = new HttpClient();
-      return await client.GetAsync(url);
+      return await httpClient.GetAsync(url);
     }
 
     private static async Task<HttpResponseMessage> GetAllMoviesInArea(int areaId)
     {
       var url = "http://www.finnkino.fi/xml/Events/?area=" + areaId;
-      using var client = new HttpClient();
-      return await client.GetAsync(url);
+      return await httpClient.GetAsync(url);
     }
 
     public static async Task<HttpResponseMessage> GetAllCurrentMovies()
     {
       var url = "http://www.finnkino.fi/xml/Events/";
-      using var client = new HttpClient();
-      return await client.GetAsync(url);
+      return await httpClient.GetAsync(url);
     }
 
     public static async Task<HttpResponseMessage> GetAllFutureMovies()
     {
       var url = "http://www.finnkino.fi/xml/Events/?listType=ComingSoon";
-      using var client = new HttpClient();
-      return await client.GetAsync(url);
+      return await httpClient.GetAsync(url);
     }
   }
 }
